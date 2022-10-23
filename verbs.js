0f14fa5f72ead07203b2faa117fab08964c2199d
James <pawson1988@me.com>
2022-10-23 21:23:13 +0200

    first commit

---
+++ verbs.js
@@ -0,0 +1,552 @@
+let verbsNotRandom = [
+    {
+        present: "dig", 
+        past: "dug", 
+        participle: "dug",
+        alternative: ""
+    },
+    {
+        present: "fall", 
+        past: "fell", 
+        participle: "fallen",
+        alternative: ""
+    },
+    {
+        present: "cut",
+        past: "cut", 
+        participle: "cut",
+        alternative: ""
+    },
+    {
+        present: "swim",
+        past: "swam", 
+        participle: "swum",
+        alternative: ""
+    },
+    {
+        present: "bite",
+        past: "bit", 
+        participle: "bitten",
+        alternative: ""
+    },
+    {
+        present: "throw",
+        past: "threw", 
+        participle: "thrown",
+        alternative: ""
+    },
+    {
+        present: "fly",
+        past: "flew", 
+        participle: "flown",
+        alternative: ""
+    },
+    {
+        present: "drive",
+        past: "drove", 
+        participle: "driven",
+        alternative: ""
+    },
+    {
+        present: "drink",
+        past: "drank", 
+        participle: "drunk",
+        alternative: ""
+    },
+    {
+        present: "eat",
+        past: "ate", 
+        participle: "eaten",
+        alternative: ""
+    },
+    {
+        present: "become",
+        past: "became", 
+        participle: "become",
+        alternative: ""
+    },
+    {
+        present: "bring",
+        past: "brought", 
+        participle: "brought",
+        alternative: ""
+    },
+    {
+        present: "build",
+        past: "built", 
+        participle: "built",
+        alternative: ""
+    },
+    {
+        present: "cost",
+        past: "cost", 
+        participle: "cost",
+        alternative: ""
+    },
+    {
+        present: "buy",
+        past: "bought", 
+        participle: "bought",
+        alternative: ""
+    },
+    {
+        present: "choose",
+        past: "chose", 
+        participle: "chosen",
+        alternative: ""
+    },
+    {
+        present: "fight",
+        past: "fought", 
+        participle: "fought",
+        alternative: ""
+    },
+    {
+        present: "get",
+        past: "got", 
+        participle: "got",
+        alternative: ""
+    },
+    {
+        present: "give",
+        past: "gave", 
+        participle: "given",
+        alternative: ""
+    },
+    {
+        present: "go",
+        past: "went", 
+        participle: "gone",
+        alternative: ""
+    },
+    {
+        present: "do",
+        past: "did", 
+        participle: "done",
+        alternative: ""
+    },
+    {
+        present: "have",
+        past: "had", 
+        participle: "had",
+        alternative: ""
+    },
+    {
+        present: "keep",
+        past: "kept", 
+        participle: "kept",
+        alternative: ""
+    },
+    {
+        present: "forget",
+        past: "forgot", 
+        participle: "forgotten",
+        alternative: ""
+    },
+    {
+        present: "freeze",
+        past: "froze", 
+        participle: "frozen",
+        alternative: ""
+    },
+    {
+        present: "feel",
+        past: "felt", 
+        participle: "felt",
+        alternative: ""
+    },
+    {
+        present: "pay",
+        past: "paid", 
+        participle: "paid",
+        alternative: ""
+    },
+    {
+        present: "shake",
+        past: "shook", 
+        participle: "shaken",
+        alternative: ""
+    },
+    {
+        present: "shoot",
+        past: "shot", 
+        participle: "shot",
+        alternative: ""
+    },
+    {
+        present: "sing",
+        past: "sang", 
+        participle: "sung",
+        alternative: ""
+    },
+    {
+        present: "break",
+        past: "broke", 
+        participle: "broken",
+        alternative: ""
+    },
+    {
+        present: "bleed",
+        past: "bled", 
+        participle: "bled",
+        alternative: ""
+    },
+    {
+        present: "bid",
+        past: "bid", 
+        participle: "bid",
+        alternative: "bidded"
+    },
+    {
+        present: "bend",
+        past: "bent", 
+        participle: "bent",
+        alternative: ""
+    },
+    {
+        present: "catch",
+        past: "caught", 
+        participle: "caught",
+        alternative: ""
+    },
+    {
+        present: "feed",
+        past: "fed", 
+        participle: "fed",
+        alternative: ""
+    },
+    {
+        present: "find",
+        past: "found", 
+        participle: "found",
+        alternative: ""
+    },
+    {
+        present: "grow",
+        past: "grew", 
+        participle: "grown",
+        alternative: ""
+    },
+    {
+        present: "leave",
+        past: "left", 
+        participle: "left",
+        alternative: ""
+    },
+    {
+        present: "lead",
+        past: "led", 
+        participle: "led",
+        alternative: ""
+    },
+    {
+        present: "lie - (to not tell the truth)",
+        past: "lied", 
+        participle: "lied",
+        alternative: ""
+    },
+    {
+        present: "lose",
+        past: "lost", 
+        participle: "lost",
+        alternative: ""
+    },
+    {
+        present: "make",
+        past: "made", 
+        participle: "made",
+        alternative: ""
+    },
+    {
+        present: "mean",
+        past: "meant", 
+        participle: "meant",
+        alternative: ""
+    },
+    {
+        present: "prove",
+        past: "proved", 
+        participle: "proved",
+        alternative: "proven"
+    },
+    {
+        present: "quit",
+        past: "quit", 
+        participle: "quit",
+        alternative: ""
+    },
+    {
+        present: "read",
+        past: "read", 
+        participle: "read",
+        alternative: ""
+    },
+    {
+        present: "ride",
+        past: "rode", 
+        participle: "ridden",
+        alternative: ""
+    },
+    {
+        present: "ring",
+        past: "rang", 
+        participle: "rung",
+        alternative: ""
+    },
+    {
+        present: "rise",
+        past: "rose", 
+        participle: "risen",
+        alternative: ""
+    },
+    {
+        present: "run",
+        past: "ran", 
+        participle: "run",
+        alternative: ""
+    },
+    {
+        present: "say",
+        past: "said", 
+        participle: "said",
+        alternative: ""
+    },
+    {
+        present: "see",
+        past: "saw", 
+        participle: "seen",
+        alternative: ""
+    },
+    {
+        present: "sell",
+        past: "sold", 
+        participle: "sold",
+        alternative: ""
+    },
+    {
+        present: "set",
+        past: "set", 
+        participle: "set",
+        alternative: ""
+    },
+    {
+        present: "send",
+        past: "sent", 
+        participle: "sent",
+        alternative: ""
+    },
+    {
+        present: "show",
+        past: "showed", 
+        participle: "shown",
+        alternative: "showed"
+    },
+    {
+        present: "dream",
+        past: "dreamed", 
+        participle: "dreamed",
+        alternative: "dreamt"
+    },
+    {
+        present: "sit",
+        past: "sat", 
+        participle: "sat",
+        alternative: ""
+    },
+    {
+        present: "sleep",
+        past: "slept", 
+        participle: "slept",
+        alternative: ""
+    },
+    {
+        present: "slide",
+        past: "slid", 
+        participle: "slid",
+        alternative: ""
+    },
+    {
+        present: "speak",
+        past: "spoke", 
+        participle: "spoken",
+        alternative: ""
+    },
+    {
+        present: "stand",
+        past: "stood", 
+        participle: "stood",
+        alternative: ""
+    },
+    {
+        present: "steal",
+        past: "stole", 
+        participle: "stolen",
+        alternative: ""
+    },
+    {
+        present: "stick",
+        past: "stuck", 
+        participle: "stuck",
+        alternative: ""
+    },
+    {
+        present: "stink",
+        past: "stank", 
+        participle: "stunk",
+        alternative: ""
+    },
+    {
+        present: "swear",
+        past: "swore", 
+        participle: "sworn",
+        alternative: ""
+    },
+    {
+        present: "take",
+        past: "took", 
+        participle: "taken",
+        alternative: ""
+    },
+    {
+        present: "think",
+        past: "thought", 
+        participle: "thought",
+        alternative: ""
+    },
+    {
+        present: "wake",
+        past: "woke", 
+        participle: "woken",
+        alternative: ""
+    },
+    {
+        present: "wear",
+        past: "wore", 
+        participle: "worn",
+        alternative: ""
+    },
+    {
+        present: "win",
+        past: "won", 
+        participle: "won",
+        alternative: ""
+    },
+    {
+        present: "write",
+        past: "wrote", 
+        participle: "written",
+        alternative: ""
+    }
+]
+
+const verbs = shuffleArray(verbsNotRandom);
+
+const container = document.querySelector(".container");
+
+for(let i = 0; i < verbs.length; i++){
+    let newDiv = document.createElement("div");
+    newDiv.setAttribute("class", "word-container");
+    container.appendChild(newDiv);
+    let word1Div = document.createElement("div");
+    word1Div.setAttribute("class", "word1");
+    newDiv.appendChild(word1Div);
+    let word2Input = document.createElement("input");
+    let word3Input = document.createElement("input");
+    word2Input.setAttribute("class", "word2");
+    word2Input.setAttribute("type", "text");
+    word2Input.setAttribute("placeholder", "Write the past verb here!");
+    word3Input.setAttribute("class", "word3");
+    word3Input.setAttribute("type", "text");
+    word3Input.setAttribute("placeholder", "Write the past participle verb here!");
+    newDiv.appendChild(word2Input);
+    newDiv.appendChild(word3Input);
+}
+
+
+const wordContainers = document.querySelectorAll(".word-container");
+const word1 = document.querySelectorAll(".word1");
+const word2 = document.querySelectorAll(".word2");
+const word3 = document.querySelectorAll(".word3");
+const score2 = document.querySelector(".score2");
+const score3 = document.querySelector(".score3");
+const totalVerbs = document.querySelector(".no-verbs");
+
+
+totalVerbs.textContent = "Total Verbs: " + verbs.length;
+let word2Score = 0;
+let word3Score = 0;
+
+let word2ScoreTracker = [];
+let word3ScoreTracker = [];
+
+    for(let i = 0; i < verbs.length; i++){ 
+        word2[i].addEventListener("input", () => {
+            if(word2[i].value.toLowerCase() === verbs[i].past.toLowerCase() || word2[i].value.toLowerCase() === verbs[i].alternative.toLowerCase()){
+                if(!word2ScoreTracker[i]){
+                    word2ScoreTracker[i] = true;
+                    word2Score++;
+                    score2.textContent = "Past Verbs: " + word2Score;
+                    verbs[i].pastDone = true;
+                }
+                console.log("word2 score: " + word2Score);
+                word2[i].style.backgroundColor = "#3fbb29";
+                word2[i].disabled = true;
+                word2[i].style.color = "white";
+                word2[i].style.textTransform = "uppercase";
+                if(word2Score === verbs.length){
+                    score2.style.backgroundColor = "blue";
+                    score2.style.color = "white";
+                    alert("You got all the past verbs!! congrats!!");
+                    
+                }
+                if(verbs[i].participleDone === true && verbs[i].pastDone === true){
+                    word2[i].parentElement.style.backgroundColor = "#a9e7b3";
+                }
+            } else {
+                word2[i].style.backgroundColor = "red";
+            }
+        }
+    )}
+
+    for(let i = 0; i < verbs.length; i++){ 
+        word3[i].addEventListener("input", () => {
+            if(word3[i].value.toLowerCase() === verbs[i].participle.toLowerCase() || word3[i].value.toLowerCase() === verbs[i].alternative.toLowerCase()){
+                if(!word3ScoreTracker[i]){
+                    word3ScoreTracker[i] = true;
+                    word3Score++;
+                    score3.textContent = "Past Participle Verbs: " + word3Score;
+                    verbs[i].participleDone = true; 
+                }
+                console.log("word3 score: " + word3Score);
+                word3[i].style.backgroundColor = "#3fbb29";
+                word3[i].disabled = true;
+                word3[i].style.color = "white";
+                word3[i].style.textTransform = "uppercase";
+                if(word3Score === verbs.length){
+                    score3.style.backgroundColor = "blue";
+                    score3.style.color = "white";
+                    alert("You got all the past participle verbs!! congrats!!");
+                }
+                if(verbs[i].participleDone === true && verbs[i].pastDone === true){
+                    word3[i].parentElement.style.backgroundColor = "#a9e7b3";
+                }
+            } else {
+                word3[i].style.backgroundColor = "red";
+            }
+        }
+    )}
+
+for(let i = 0; i < verbs.length; i++){
+        word1[i].textContent = verbs[i].present.toUpperCase();
+}
+
+function shuffleArray(array){
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    return array;
+  }
+  
